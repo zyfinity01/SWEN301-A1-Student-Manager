0487830c86a0a11e2d260f7f20f6d03c5f0f7e7e
Niraj Gandhi <ngandhi360@gmail.com>
2023-07-26 11:59:05 +1200

    Pull in changes from R1.0.3 Assignment template

--- src/main/java/nz/ac/wgtn/swen301/assignment1/cli/StudentManagerUI.java
+++ src/main/java/nz/ac/wgtn/swen301/assignment1/cli/StudentManagerUI.java
@@ -4,10 +4,10 @@ public class StudentManagerUI {
 
     // THE FOLLOWING METHOD MUST BE IMPLEMENTED
     /**
-     * Executable: the user will provide a student id as single argument, and if the details are found,
-     * the respective details are printed to the console.
-     * E.g. a user could invoke this by running "java -cp <someclasspath> nz.ac.wgtn.swen301.assignment1.cli.FindStudentDetails id42"
+     * Executable: the user will provide argument(s) and print details to the console as described in the assignment brief,
+     * E.g. a user could invoke this by running "java -cp <someclasspath> <arguments></arguments>"
      * @param arg
      */
     public static void main (String[] arg) {}
 }
+
