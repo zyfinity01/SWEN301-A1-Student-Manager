bf409971e8f11fdd549315b3ca6f791047f1126d
Niraj Gandhi <ngandhi360@gmail.com>
2023-08-09 11:59:31 +1200

    remove unnecessary imports

--- src/main/java/nz/ac/wgtn/swen301/assignment1/cli/StudentManagerUI.java
+++ src/main/java/nz/ac/wgtn/swen301/assignment1/cli/StudentManagerUI.java
@@ -8,11 +8,7 @@ import org.apache.commons.cli.*;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class StudentManagerUI {
 
